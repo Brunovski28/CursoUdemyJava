a8db18dd45a4d1f0e063974c8f3996c3a9c93379
Bruno Camilo <bruno.camilo2004@gmail.com>
2024-07-22 20:28:59 -0300

    Adicionando herança

---
+++ exercicios/_03avancado/herancaEPolimorfismo/_01HerancaComContaDeBanco/Main.java
@@ -0,0 +1,9 @@
+package exercicios._03avancado.herancaEPolimorfismo._01HerancaComContaDeBanco;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ exercicios/_03avancado/herancaEPolimorfismo/_01HerancaComContaDeBanco/entities/Account.java
@@ -0,0 +1,50 @@
+package exercicios._03avancado.herancaEPolimorfismo._01HerancaComContaDeBanco.entities;
+
+public class Account {
+
+    private Integer number;
+    private String holder;
+    protected double balance;
+
+    public Account() {
+    }
+
+    public Account(Integer number, String holder, double balance) {
+        this.number = number;
+        this.holder = holder;
+        this.balance = balance;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void withdraw(double amount){
+        this.balance -= amount;
+    }
+
+    public void deposit(double amount){
+        this.balance += amount;
+    }
+
+}
