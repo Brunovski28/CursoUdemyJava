5f1f929f300bf9ca24087865c4803c1c38e49d8e
Bruno Camilo <bruno.camilo2004@gmail.com>
2024-07-26 06:02:32 -0300

    Aprendendo classe abstrato

---
+++ exercicios/_03avancado/herancaEPolimorfismo/_04Abstrato/entities/BusinessAccount.java
@@ -0,0 +1,37 @@
+package exercicios._03avancado.herancaEPolimorfismo._04Abstrato.entities;
+
+import exercicios._03avancado.herancaEPolimorfismo._01HerancaComContaDeBanco.entities.Account;
+
+public class BusinessAccount extends Account {
+
+    private double loanLimit;
+
+    public BusinessAccount(){
+    }
+
+    public BusinessAccount(Integer number, String holder, double balance, double loanLimit) {
+        super(number, holder, balance); // o Super ele chama os dados da superclasse, que seria o account
+        this.loanLimit = loanLimit;
+    }
+
+    public double getLoanLimit() {
+        return loanLimit;
+    }
+
+    public void setLoanLimit(double loanLimit) {
+        this.loanLimit = loanLimit;
+    }
+
+    public void loan(double amount){
+        if (amount <= loanLimit){
+            balance += amount - 10;
+        }
+    }
+
+    @Override // Esse comando sobrescreve por cima o método que tem na superclasse
+    public void withdraw(double amount){
+        super.withdraw(amount); // usando o withdraw da superclasse
+        balance -= 2;
+    }
+
+}
---
+++ exercicios/_03avancado/herancaEPolimorfismo/_04Abstrato/entities/SavingsAccount.java
@@ -0,0 +1,36 @@
+package exercicios._03avancado.herancaEPolimorfismo._04Abstrato.entities;
+
+import exercicios._03avancado.herancaEPolimorfismo._01HerancaComContaDeBanco.entities.Account;
+
+// o comando FINAL, não permite que não tenha uma subclasse
+public /*final*/class SavingsAccount extends Account {
+
+    private double interestRate;
+
+    public SavingsAccount(){
+    }
+
+    public SavingsAccount(Integer number, String holder, double balance, double interestRate) {
+        super(number, holder, balance);
+        this.interestRate = interestRate;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public void updateBalance(){
+        balance += balance * interestRate;
+    }
+
+    @Override // Esse comando sobrescreve por cima o método que tem na superclasse
+    // Final não permiti que as subclasses use esse método para sobrepor
+    public final void withdraw(double amount){
+        this.balance -= amount;
+    }
+
+}
